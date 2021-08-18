270613a25d79dc773c1039239d29b055c22fbfe5
Yuval Dadon <dadon1365@gmail.com>
2021-08-18 15:06:27 +0000

    Added query params from home to sign up page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
+    # Get request home
     def home
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
+    # Get request /about
     def about
     end
 end
