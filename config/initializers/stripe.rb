10405fd7640fb87d71970d5e55de1c758c655889
Yuval Dadon <dadon1365@gmail.com>
2021-08-19 15:57:53 +0000

    Added server side code for creating subscriptions for premium users

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,9 @@
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
+#Stripe.setPublishableKey(STRIPE_PUBLIC_KEY)
+
+#Rails.configuration.stripe = {
+#  publishable_key: Rails.application.secrets.stripe_publishable_key,
+#  secret_key: Rails.application.secrets.stripe_secret_key
+#}
+#Stripe.api_key = Rails.configuration.stripe[:secret_key]
